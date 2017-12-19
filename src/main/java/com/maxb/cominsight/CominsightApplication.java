7bf39abcf120a18f157525208ad4ab47749c6b6c
MaxB <maxbpro2009@gmail.com>
2017-12-19 19:45:50 +0300

    Added spring boot project

---
+++ src/main/java/com/maxb/cominsight/CominsightApplication.java
@@ -0,0 +1,12 @@
+package com.maxb.cominsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CominsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CominsightApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maxb/cominsight/CominsightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maxb.cominsight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CominsightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
